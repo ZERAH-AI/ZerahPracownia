781102193ce32bb0e40dcad18fabec513d8c7980
ZERAH-AI <mail@zerah.online>
2025-07-11 00:36:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const CONFIG = {
         workspaceId: process.env.DUST_WORKSPACE_ID || 'VZuYxk8oJc',
         apiKey: process.env.DUST_API_KEY || 'sk-4c091a74340924f59ac24ffd77004b2d',
         agentName: process.env.DUST_NAME || 'ZERAH',
-        agentId: 'XxANDnN74a', // Agent ID
+        agentId: 'mhmm8HSWpr', // Agent ID
         baseUrl: 'https://dust.tt/api/v1'
     },
     // Chatwoot Configuration
